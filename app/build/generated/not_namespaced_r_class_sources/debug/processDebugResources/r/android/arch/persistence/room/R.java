20b725aac5a4887c38876ec6cf6593b79732301e
jennhe <jenhe2000@gmail.com>
2019-07-10 01:15:22 -0700

    added refresh and endless scrolling

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/arch/persistence/room/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/arch/persistence/room/R.java
@@ -93,16 +93,16 @@ public final class R {
         public static final int italic = 0x7f070050;
         public static final int line1 = 0x7f070054;
         public static final int line3 = 0x7f070055;
-        public static final int normal = 0x7f07005f;
-        public static final int notification_background = 0x7f070060;
-        public static final int notification_main_column = 0x7f070061;
-        public static final int notification_main_column_container = 0x7f070062;
-        public static final int right_icon = 0x7f07006b;
-        public static final int right_side = 0x7f07006c;
-        public static final int text = 0x7f070091;
-        public static final int text2 = 0x7f070092;
-        public static final int time = 0x7f070095;
-        public static final int title = 0x7f070096;
+        public static final int normal = 0x7f070060;
+        public static final int notification_background = 0x7f070061;
+        public static final int notification_main_column = 0x7f070062;
+        public static final int notification_main_column_container = 0x7f070063;
+        public static final int right_icon = 0x7f07006c;
+        public static final int right_side = 0x7f07006d;
+        public static final int text = 0x7f070093;
+        public static final int text2 = 0x7f070094;
+        public static final int time = 0x7f070097;
+        public static final int title = 0x7f070098;
     }
     public static final class integer {
         private integer() {}
--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -120,4 +120,16 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
 //            tvReply = itemView.findViewById(R.id.tvReply);
         }
     }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        mTweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> list) {
+        mTweets.addAll(list);
+        notifyDataSetChanged();
+    }
 }
